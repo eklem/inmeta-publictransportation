99b9c3c9d37c792fbee60a25452e469fc46f53f2
Espen Klem <espen.klem@gmail.com>
2017-02-06 04:26:57 -0500

    Adding static example. Need to wait for cors-support for further development

---
+++ index.js
@@ -0,0 +1,21 @@
+var vm = new Vue({
+  el: '#app',
+  data: {
+    departures: []
+  },
+  methods: {
+    initialScreen() {
+      url = 'http://reisapi.ruter.no/StopVisit/GetDepartures/3012130?datetime=017-02-03T13:18&transporttypes=Metro&linenames=5'
+      axios.get(url, {responseType: 'blob'})
+        .then(function (response) {
+          console.log(JSON.stringify(response))
+        })
+        .catch(function (error) {
+          console.log(error)
+        })
+    }
+  },
+  mounted() {
+    this.initialScreen()
+  }
+})
