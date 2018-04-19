73d4eddecf337b971a642f9147696c760bb5b3c1
Espen Klem <espen.klem@gmail.com>
2018-04-19 10:09:45 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var vm = new Vue({
   },
   methods: {
     initialScreen() {
-      url = 'http://reisapi.ruter.no/StopVisit/GetDepartures/3012130?datetime=017-02-03T13:18&transporttypes=Metro&linenames=5'
+      url = 'https://reisapi.ruter.no/StopVisit/GetDepartures/3012130?datetime=017-02-03T13:18&transporttypes=Metro&linenames=5'
       axios.get(url, {responseType: 'blob'})
         .then(function (response) {
           console.log(JSON.stringify(response))
